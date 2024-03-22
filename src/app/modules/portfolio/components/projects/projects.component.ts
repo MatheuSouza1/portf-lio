f89b68f53fc0dd6a73e43bbd81ceee757a3533c7
Matheus <rdesouzamatheus@gmail.com>
2024-03-22 09:44:46 -0300

    Criado componentes, seus HTML's, CSS e lógicas em TS.

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-projects',
@@ -22,6 +24,20 @@ export class ProjectsComponent {
           href: "https://github.com/MatheuSouza1/ApiMessageDDD"
         }
       ]
-    }
+    },
+    {
+      src: "assets/icons/projects/apiMessage.jpg",
+      alt: "Imagem da Api de menssagens",
+      title: "Api menssagem",
+      width: "160px",
+      height: "80px",
+      description:"",
+      links: [
+        {
+          name: "Api Menssagem",
+          href: "https://github.com/MatheuSouza1/ApiMessageDDD"
+        }
+      ]
+    },
   ])
 }
